a90ec6331a5f4c22807b919113b688cd18fedfb5
Stefano <stefano.pisciella1@student.univaq.it>
2024-11-02 22:25:21 +0100

    added the publication date filter

--- main.py
+++ main.py
@@ -62,6 +62,32 @@ def print_prettified_json(website, title, link_to_detail_page, publication_date,
 
     print(json.dumps(announcement, indent=4))
 
+def check_if_the_announcement_must_be_scraped(is_a_disim_announcement, announcement_publication_date, announcement_url):
+    json_filename = "disim_db.json" if is_a_disim_announcement else "adsu_db.json"
+
+    with open(json_filename, 'r', encoding='utf-8') as json_file:
+        json_content = json.load(json_file)
+
+        if announcement_publication_date < json_content["last_scraped_announcement_publication_date"]:
+            # the announcement to be checked is older than the last scraped announcement
+            return False
+        elif announcement_publication_date > json_content["last_scraped_announcement_publication_date"]:
+            # the announcement to be checked is newer than the last scraped announcement
+            return True
+        elif announcement_publication_date == json_content["last_scraped_announcement_publication_date"]:
+            # retrive all the urls relative to all announcement published on the disim website on the day of announcement_publication_date
+            announcements_urls = json_content["announcements_urls"]
+
+            if announcement_url in announcements_urls:
+                # the announcement to be checked has been already scraped
+                return False
+            else:
+                # the announcement to be checked hasn't been scraped yet
+                return True
+
+def write_db_file(json_filename, data):
+    with open(json_filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4)
 
 def scrape_disim_website():
     domain = "https://www.disim.univaq.it/"
@@ -72,10 +98,15 @@ def scrape_disim_website():
     announcements = homepage('#annunci > div.two-thirds.column > div.row')
 
     for announcement in announcements:
-        title = pq(announcement).find("h5 > a").text()
-        link_to_detail_page = domain + reformat_url(pq(announcement).find("h5 > a").attr("href"))
         publication_date = pq(announcement).find("p.post_meta > span.calendar").text()
         reformatted_publication_date = reformat_date(publication_date)
+        link_to_detail_page = domain + reformat_url(pq(announcement).find("h5 > a").attr("href"))
+
+        if not check_if_the_announcement_must_be_scraped(True, reformatted_publication_date, link_to_detail_page):
+            # in the website the announcements are sorted from the most recent to the least recent ==> if the first announcement must not be scraped, then also the others must not
+            break
+
+        title = pq(announcement).find("h5 > a").text()
         announcement_tags = get_announcement_tags(pq(announcement).find("p.post_meta > span.tags > a"))
         preview_of_the_announcement_content = pq(announcement).find("p:nth-child(2)").text()
 
@@ -94,11 +125,15 @@ def scrape_adsu_website():
     announcements = news_section('#main > div.container > div.row > div.col-lg-8.col-md-8.col-sm-12.col-xs-12 > div.col_in.__padd-right > div.posts_list.with_sidebar > ul.post_list_ul > li')
 
     for announcement in announcements:
-        title = pq(announcement).find("h4 > a").text()
-        link_to_detail_page = pq(announcement).find("h4 > a").attr("href")
-
         publication_date = pq(announcement).find("div.stm_post_details > ul > li.post_date").text()
         reformatted_publication_date = reformat_date(publication_date)
+        link_to_detail_page = pq(announcement).find("h4 > a").attr("href")
+
+        if not check_if_the_announcement_must_be_scraped(False, reformatted_publication_date, link_to_detail_page):
+            # in the website the announcements are sorted from the most recent to the least recent ==> if the first announcement must not be scraped, then also the others must not
+            break
+
+        title = pq(announcement).find("h4 > a").text()
 
         announcement_tags = pq(announcement).find("div.stm_post_details > ul > li.post_cat > span").text()
         announcement_tags = announcement_tags.split(', ')
@@ -114,4 +149,6 @@ def scrape_adsu_website():
 
 if __name__ == '__main__':
     scrape_disim_website()
-    scrape_adsu_website()
+
+    # CHECK
+    # scrape_adsu_website()
