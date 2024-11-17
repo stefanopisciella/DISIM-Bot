10dc66f42201f89289f23281d73792302e461e47
Stefano <stefano.pisciella1@student.univaq.it>
2024-11-18 00:13:45 +0100

    added configuration_file.py

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from website_scrapers.WebsiteScraper import WebsiteScraper
 from website_scrapers.DISIMwebsiteScraper import DISIMwebsiteScraper
 from website_scrapers.ADSUwebsiteScraper import ADSUwebsiteScraper
 
+import configuration_file as conf
+
+import time
+
+SECONDS_IN_ONE_HOUR = 3600
 
 if __name__ == '__main__':
     disim = DISIMwebsiteScraper()
     adsu = ADSUwebsiteScraper()
 
-    announcements_to_be_published = disim.get_announcements()
-    announcements_to_be_published.extend(adsu.get_announcements())
+    while True:
+        announcements_to_be_published = disim.get_announcements()
+        announcements_to_be_published.extend(adsu.get_announcements())
 
-    WebsiteScraper.debug_by_printing_prettified_json(announcements_to_be_published)
+        WebsiteScraper.debug_by_printing_prettified_json(announcements_to_be_published)
+
+        time.sleep(conf.HOURS_BETWEEN_SCRAPING * SECONDS_IN_ONE_HOUR)
