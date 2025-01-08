c7dbe5cc17111bc69d3e2d12b8949eaa47d9232f
Stefano <stefano.pisciella1@student.univaq.it>
2025-01-08 21:23:16 +0100

    moved main.py content to BroadcastBot.py

--- BroadcastBot.py
+++ BroadcastBot.py
@@ -2,6 +2,13 @@ import logging
 from telegram import Bot
 from telegram.error import TelegramError
 
+import time
+import asyncio
+
+from website_scrapers.DISIMwebsiteScraper import DISIMwebsiteScraper
+from website_scrapers.ADSUwebsiteScraper import ADSUwebsiteScraper
+
+from model.Announcement import Announcement as AnnouncementModel
 from model.User import User as UserModel
 from model.UninterestedWebsite import UninterestedWebsite as UninterestedWebsiteModel
 
@@ -9,6 +16,10 @@ import configuration_file as conf
 
 
 class BroadcastBot:
+    # START constants
+    SECONDS_IN_ONE_HOUR = 3600
+    # END constants
+
     # START singleton design pattern
     _instance = None
 
@@ -29,6 +40,31 @@ class BroadcastBot:
         return cls._instance
 
     # END singleton design pattern
+
+
+    async def run(self):
+        disim = DISIMwebsiteScraper()
+        adsu = ADSUwebsiteScraper()
+
+        while True:
+            # START scrape announcements from "disim" and "adsu" websites
+            announcements_to_be_published = disim.get_announcements()
+            announcements_to_be_published.extend(adsu.get_announcements())
+
+            # WebsiteScraper.debug_by_printing_prettified_json(announcements_to_be_published)
+            # END scrape announcements from "disim" and "adsu" websites
+
+            # START insert announcements into SQLite DB
+            announcement_model = AnnouncementModel()
+            announcement_model.bulk_insert(announcements_to_be_published)
+            # END insert announcements into SQLite DB
+
+            broadcast_bot = BroadcastBot()
+            await broadcast_bot.send_announcements_filtered_by_tags_of_interest_to_user(announcements_to_be_published)
+
+            del announcements_to_be_published  # free up RAM
+
+            time.sleep(conf.HOURS_BETWEEN_SCRAPING * self.SECONDS_IN_ONE_HOUR)
 
     async def send_announcements_filtered_by_tags_of_interest_to_user(self, announcements):
         users = UserModel.get_all()
@@ -86,3 +122,12 @@ class BroadcastBot:
         ]
 
         return '\n'.join(sections)
+
+
+if __name__ == '__main__':
+    bot = BroadcastBot()
+    asyncio.run(bot.run())
+
+
+
+
